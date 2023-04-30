1e45a76d222a3d6e6469cfb2edc25450d9018353
Zachary Sirna <zsirna@yahoo.com>
2023-04-30 19:59:12 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Zac Sirna Portfolio</h1>
       </header>
+      <div>
+        <h1>About</h1>
+        <div >
+          <img className='App-picture' src="Second Temporary Headshot.PNG" alt="me"/>
+          </div>
+        <div className='App-text'>
+        <p>I am an experienced Full Stack Developer with a strong background in frontend and backend 
+          languages such as Ruby, Ruby on Rails, JavaScript, and React.js. As a musician with a passion 
+          for audio production, I transitioned to a career as a lead AV Technician, bringing over 7 years 
+          of experience in problem-solving and collaboration to each project. My unique perspective allows 
+          me to approach challenges with innovative ideas and creativity, resulting in robust and high-quality 
+          solutions. As a proactive and motivated worker, I am committed to exceeding expectations and 
+          delivering exceptional results.</p>
+          </div>
+      </div>
+      <div>
+        <h1>Projects</h1>
+        <a href="https://friendo.onrender.com/" target="_blank" rel="noreferrer">
+        Friendo
+      </a>{" "}
+        <br/>
+        <br/>
+        <a href="https://co-opper.onrender.com/" target="_blank" rel="noreferrer">
+        Co-opper
+      </a>{" "}
+        <br/>
+        <br/>
+      <a href="https://zander618.github.io/GhibliRepo/" target="_blank" rel="noreferrer">
+        GhibliRepo
+      </a>{" "}
+      </div>
+      <div>
+        <h1>Contact</h1>
+        <p>Email: Zsirna@gmail.com</p>
+      </div>
     </div>
   );
 }
