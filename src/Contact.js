bc2943c7dfff42621f879f90a15aa34ee94f83d0
Zachary Sirna <zsirna@yahoo.com>
2023-05-23 17:35:36 -0400

    updating contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
     <div>
-    <h1>Contact</h1>
-    <p>Email: Zsirna@gmail.com</p>
-  </div>
-  )
-}
+      <h1>Contact</h1>
+      <h2>Email</h2>
+      <p>Zsirna@gmail.com</p>
+      <h2>Message me through LinkedIn</h2>
+      <div>
+        <a
+          href="https://www.linkedin.com/in/zachary-sirna/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Zac Sirna
+        </a>{" "}
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
